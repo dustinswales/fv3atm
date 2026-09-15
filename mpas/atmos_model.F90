7cd345205a22fce3551ecb8bde1e509e5d9834ac
Grant Firl <grant.firl@noaa.gov>
2026-09-15 11:28:24 -0400

    a little cleanup

--- mpas/atmos_model.F90
+++ mpas/atmos_model.F90
@@ -292,7 +292,7 @@ contains
     ! we are calling the physics before the MPAS dynamical core.
     call ufs_mpas_grid_to_physics(UFSATM_grid)
     call ufs_mpas_sfc_to_physics(UFSATM_sfcprop, UFSatm_control)
-    !call ufs_mpas_gwd_to_physics(UFSATM_control, UFSATM_sfcprop)
+    call ufs_mpas_gwd_to_physics(UFSATM_control, UFSATM_sfcprop)
     call ufs_mpas_to_physics(UFSATM_statein, UFSATM_sfcprop, UFSATM_radtend)
 
     ! Register CCPP
